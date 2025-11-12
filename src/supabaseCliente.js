bbcc74a9cabc351d297463b30e650ce2c7ddf7c1
Mássimo Quirolo <massimoaquirolo@gmail.com>
2025-11-12 17:52:21 -0300

    Implementar variables de entorno (.env) por seguridad

--- src/supabaseCliente.js
+++ src/supabaseCliente.js
@@ -7,12 +7,11 @@ import { createClient } from '@supabase/supabase-js'
 // 2. Copia la 'anon' 'public' key (la llave pública anónima)
 // ---------------------------------
 
-const SUPABASE_URL = 'https://ecewvwiwkqtjylwyeemd.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjZXd2d2l3a3F0anlsd3llZW1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4ODU3NzAsImV4cCI6MjA3ODQ2MTc3MH0.HEc4EJ3j_BLSnE1mYjs2Q0QGUqFtpceVQn9cJ7ZqOLc';
+// Lee las variables desde 'import.meta.env' (así lo hace Vite)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Creamos el cliente de Supabase
-// Esta variable 'supabase' es nuestra puerta de entrada a la base de datos.
-// La exportamos para poder usarla en cualquier parte de nuestra aplicación.
+// Creamos el cliente de Supabase (esto sigue igual)
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 export default supabase
